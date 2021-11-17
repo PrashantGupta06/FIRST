9bdd8451f14422010f7edc5a791264d35cca861e
Prashant Gupta <prashantgupta7887@gmail.com>
2021-11-17 18:05:15 +0530

    initial

---
+++ day2.js
@@ -0,0 +1,22 @@
+function abcd()
+{
+    console.log("hello");
+}
+abcd();
+const student ={
+    name:"Prashant",
+    branch:"cs",
+};
+console.log(student.name);
+const array1=["a","b"];
+const array2=["c","d"];
+let array3=[...array1,...array2];
+console.log(array3)
+let p=4;
+console.log(p);
+let b=56;
+console.log(b);
+let d=45;
+console.log(d);
+let z=0;
+console.log(z);
---
+++ newone.js
@@ -0,0 +1,33 @@
+console.log("hello world");
+function abcd()
+{
+    console.log("i am funtion");
+
+}
+let k=45;
+console.log(k);
+
+let j;
+console.log(typeof j);
+
+let mystring="prashant gupta";
+console.log(mystring);
+mystring="My string";
+console.log( mystring);
+let name1=[1,2,3,true,"hello"];
+console.log(name1);
+console.log(name1[1]);
+name1.push(99);
+name1.pop();
+console.log(name1);
+let popped = name1.pop();
+console.log(popped);
+let num=[1,2,3,4,6,7,8,7];
+let evennum=[];
+for(let i=0;i<num.length;i++)
+if(num[i]%2===0) evennum.push();
+console.log(evennum);
+let m=23;
+console.log(m);
+let c=23;
+console.log(c);
